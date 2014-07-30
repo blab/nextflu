41a76962dc6e823fc4cd68eed39748745283e1c5
Trevor Bedford <trvrb@trevorbedford.com>
2014-07-29 23:12:07 -0700

    Add more clicks to GISAID script.

--- augur/data/gisaid.py
+++ augur/data/gisaid.py
@@ -30,13 +30,37 @@ time.sleep(10)
 species = driver.find_element_by_id('ce_n7h14c_86_select')
 species.send_keys("H")
 time.sleep(10)
+start_date = driver.find_element_by_id('ce_n7h14c_8a_input')
+start_date.send_keys("2010-01-01")
+time.sleep(10)
 button = driver.find_element_by_xpath("//button[@accesskey='g']")
 button.click()
 
-# download
+# select sequences
 time.sleep(10)
 checkbox = driver.find_element_by_xpath("//span[@class='yui-dt-label']/input[@type='checkbox']")
 checkbox.click()
 time.sleep(30)
-button = driver.find_element_by_xpath("//div[@id='ce_n7h14c_9f']/button")
-button.click() # this is failing because of the 20k isolate limit
+button = driver.find_element_by_xpath("//div[@id='ce_n7h14c_9f']//button")
+button.click()
+
+# set download options
+time.sleep(10)
+driver.switch_to_frame(driver.find_element_by_tag_name("iframe"))
+checkbox = driver.find_element_by_xpath("//input[@name='ce_n7h14c_9k_name' and @value='dna']")
+checkbox.click()
+time.sleep(10)
+checkbox = driver.find_element_by_xpath("//input[@name='ce_n7h14c_9m_name' and @value='HA']")
+checkbox.click()
+
+# download
+# attempt to prevent download dialog (not working)
+time.sleep(10)
+profile = webdriver.FirefoxProfile()
+profile.set_preference('browser.download.folderList', 2) # custom location
+profile.set_preference('browser.download.manager.showWhenStarting', False)
+profile.set_preference('browser.download.dir', '/tmp')
+profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/fasta')
+button = driver.find_element_by_xpath("//div[@id='ce_n7h14c_9y']//button")
+button.click()
+
