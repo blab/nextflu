cdd16ca191d27a99a586841ece1c691387143e71
Richard Neher <richard@ag-neher-xps13.(none)>
2014-09-28 20:31:02 +0200

    changed time interval in for T2 calculation in tree_clean.py.

--- augur/tree_refine.py
+++ augur/tree_refine.py
@@ -164,7 +164,7 @@ def add_node_attributes(tree):
 			
 def add_LBI(tree):
 	print "calculate local branching index"
-	T2 = get_average_T2(tree, 1)
+	T2 = get_average_T2(tree, 365)
 	tau =  T2*2**-4
 	print "avg pairwise distance:", T2
 	print "memory time scale:", tau
