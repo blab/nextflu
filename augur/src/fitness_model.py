ce7998b64271e4bdbf038e9e222dfdb14184835c
Richard Neher <richard@ag-neher-xps13.(none)>
2015-01-30 22:54:52 +0100

    added comments

--- augur/src/fitness_model.py
+++ augur/src/fitness_model.py
@@ -28,6 +28,13 @@ class fitness_model(object):
 						for y in xrange(ymin, ymax)]
 
 	def calc_tip_counts(self):
+		'''
+		goes over all nodes and assigns tips to seasons (or any interval, really)
+		then counts tips in every season for all internal nodes and calculates the
+		fraction of viruses the clade defined by the internal node accounts for in 	
+		a given season, i.e. the frequency 
+		'''
+		# count tips
 		for node in self.tree.postorder_node_iter():
 			node.tip_counts = defaultdict(int)
 			for child in node.child_nodes():
@@ -39,10 +46,14 @@ class fitness_model(object):
 				if len(tmp_season_list)==1:
 					node.tip_counts[tmp_season_list[0]]=1
 
+		# short cut to total number of tips per seaons
 		total_counts = self.tree.seed_node.tip_counts
+		# prune seasons where few observations were made, only consecutive pairs
+		# with sufficient tip count are retained
 		self.test_fit_season_pairs = [(s,n) for s,n in izip(self.seasons[:-1], self.seasons[1:]) 
 							if total_counts[s]>min_tips and total_counts[n]>min_tips]
 
+		# calculate frequencies
 		for node in self.tree.postorder_node_iter():
 			node.frequencies = defaultdict(float)
 			for season, count in node.tip_counts.iteritems():
@@ -55,6 +66,8 @@ class fitness_model(object):
 			func(self.tree.seed_node, attr = pred)
 
 	def make_flat_lists_per_season(self):
+		# for each pair of seaons with sufficient tip counts, 
+		# make a list of clades that are to be used in model fitting
 		self.clades_in_season = []  # list of clades for each season...
 		for s,t in self.test_fit_season_pairs:
 			tmp_clades = []
@@ -63,6 +76,7 @@ class fitness_model(object):
 					tmp_clades.append(node)
 			self.clades_in_season.append(tmp_clades)
 
+		# calculate the predictors for the clades used for model fitting
 		self.freq_and_predictors = []
 		for (s,t), clades in izip(self.test_fit_season_pairs, self.clades_in_season):
 			tmp_freq = [[x.frequencies[s], x.frequencies[t]] for x in clades]
@@ -102,6 +116,6 @@ class fitness_model(object):
 if __name__=="__main__":
 	from io_util import *
 	from tree_util import *
-	tree_fname='data/tree.json'
+	tree_fname='data/tree_ancestral.json'
 	tree =  json_to_dendropy(read_json(tree_fname))
 	fm = fitness_model(tree)
