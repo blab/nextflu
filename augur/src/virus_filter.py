cbc8d7a9750258c79c1ef7efd5255908dbc02c78
Richard Neher <richard@ag-neher-xps13.(none)>
2015-02-24 18:40:12 +0100

    first shot at modular virus processing

--- augur/src/virus_filter.py
+++ augur/src/virus_filter.py
@@ -7,198 +7,190 @@
 
 import os, re, time, datetime, csv, sys
 from io_util import *
+from collections import defaultdict
 sys.path.append('../source-data')
-from H3N2_outgroup_and_vaccine import outgroup, vaccine_strains
+from H3N2_outgroup_and_vaccine import outgroup, vaccine_strains, min_length
 
-def parse_gisaid(fasta):
-	"""Parse FASTA file from GISAID with default header formating"""
-	viruses = []
-	try:
-		handle = open(fasta, 'r')
-	except IOError:
-		print fasta + " not found"
-	else:
-		for record in SeqIO.parse(handle, "fasta"):
-			words = record.description.replace(">","").replace(" ","").split('|')
-			strain = words[0]
-			accession = words[1]
-			passage = words[3]
-			date = words[5]
-			seq = str(record.seq).upper()
-			v = {
-				"strain": strain,
-				"date": date,
-				"accession": accession,
-				"db": "GISAID",
-				"seq": seq
-			}
-			if passage != "":
-				v['passage'] = passage
-			viruses.append(v)
-		handle.close()
 
-	return viruses
+class virus_filter(object):
 
-def sort_length(viruses):
-	"""Sort by length, but randomize viruses of a given length"""
-	from random import shuffle
-	shuffle(viruses)
-	return sorted(viruses, key = lambda v: len(v['seq']), reverse = True)
+	def __init__(self,viruses=None):
+		if viruses is None: viruses=[]
+		self.viruses = viruses
+		self.strain_lookup = {}
+		self.outgroup = None
 
-def fix_strain_names(viruses):
-	for v in viruses:
-		v['strain'] = v['strain'].replace('\'','').replace('(','').replace(')','').replace('H3N2','').replace('Human','').replace('human','').replace('//','/')
+	def filter_generic(self, min_length=None, date_spec = 'full', prepend_strains = None):
+		'''
+		filter viruses by length and accurate date, sort, add additioanl strains such
+		as vaccine strains that are preferentially retained and prune to unique strains
+		'''
+		print len(self.viruses) + " initial viruses"
+		if min_length is not None:
+			self.filter_length(min_length)
+			print len(self.viruses) + " after filtering by length >=", min_length
 
-def filter_strain_names(viruses):
-	filtered_viruses = filter(lambda v: re.match(r'^A/', v['strain']) != None, viruses)
-	return filtered_viruses
+		self.filter_date(min_length, date_spec)
+		print len(self.viruses) + " after filtering for precise dates"
+		self.sort_length()
+		if prepend_strains is not None:
+			self.viruses = prepend_strains + self.viruses
+		self.filter_unique()
+		print len(self.viruses) + " after filtering for unique strains"
 
-def filter_length(viruses):
-	return filter(lambda v: len(v['seq']) >= 987, viruses)
+	def sort_length(self):
+		'''	
+		Sort by length, but randomize viruses of a given length
+		'''	
+		from random import shuffle
+		shuffle(self.viruses)
+		self.viruses.sort(key = lambda v: len(v['seq']), reverse = True)
 
-def filter_date(viruses):
-	return filter(lambda v: re.match(r'\d\d\d\d-\d\d-\d\d', v['date']) != None, viruses)
+	def filter_unique(self):
+		'''		
+		Keep only the first isolate of a strain
+		'''	
+		filtered_viruses = []
+		for v in viruses:
+			label = v['strain'].lower() 
+			if not label in self.strain_lookup:
+				filtered_viruses.append(v)
+				self.strain_lookup[label]=v
+		self.viruses=filtered_viruses
 
-def filter_passage(viruses):
-	round_one = filter(lambda v: re.match(r'^E\d+', v.get('passage',''), re.I) == None, viruses)
-	return filter(lambda v: re.match(r'^Egg', v.get('passage',''), re.I) == None, round_one)
+	def filter_length(self, min_length):
+		self.viruses = filter(lambda v: len(v['seq']) >= min_length, self.viruses)
 
-def filter_unique(viruses):
-	"""Keep only the first isolate of a strain"""
-	filtered_viruses = {}
-	for v in viruses:
-		label = v['strain'].lower() 
-		if not label in filtered_viruses:
-			filtered_viruses[label] = v
-	return filtered_viruses.values()
+	def filter_date(self, date_spec):
+		if date_spec=='full':
+			self.viruses = filter(lambda v: re.match(r'\d\d\d\d-\d\d-\d\d', v['date']) != None, self.viruses)
+		elif date_spec=='year':
+			self.viruses = filter(lambda v: re.match(r'\d\d\d\d', v['date']) != None, self.viruses
 
-def append_country_and_region(viruses):
-	"""Label viruses with geographic location based on strain name"""
-	"""Location is to the level of country of administrative division when available"""
-	reader = csv.DictReader(open("source-data/geo_synonyms.tsv"), delimiter='\t')		# list of dicts
-	label_to_country = {}
-	for line in reader:
-		label_to_country[line['label'].lower()] = line['country']
-	for v in viruses:
-		label = re.match(r'^A/([^/]+)/', v['strain']).group(1).lower()					# check first for whole geo match
-		v['country'] = 'Unknown'
-		if label in label_to_country:
-			v['country'] = label_to_country[label]
+	def subsample(self, years_back, viruses_per_month, prioritize = None):
+		'''
+		Subsample x viruses per month
+		Take from beginning of list - this will prefer longer sequences
+		Take viruses 1 per region in a cycle to get geographic diversity
+		But pad with additional viruses from populous regions if necessary
+		'''
+		if prioritize is None:
+			prioritize=[]
 		else:
-			label = re.match(r'^A/([^\-^\/]+)[\-\/]', v['strain']).group(1).lower()		# check for partial geo match
+			prioritize = [v.lower() for v in prioritize]
+
+		priority_viruses = viruses_by_date_region([v for label,v in self.strain_lookup.iteritems() if v in prioritize]) 
+		other_viruses = viruses_by_date_region([v for label,v in self.strain_lookup.iteritems() if v not in prioritize]) 
+
+		filtered_viruses = []
+		first_year = datetime.datetime.today().year - years_back
+		first_month = datetime.datetime.today().month
+		regions = list(set([v['region'] for v in self.viruses]))
+
+		print "Filtering between " + str(first_month) + "/" + str(first_year) + " and today"
+		print "Selecting " + str(viruses_per_month) + " viruses per month"
+		y = first_year
+		for m in range(first_month,13):
+			filtered_viruses.extend(select_viruses(priority_viruses,other_viruses, 
+												y, m, viruses_per_month, regions))
+		for y in range(first_year+1,datetime.datetime.today().year+1):
+			filtered_viruses.extend(select_viruses(priority_viruses,other_viruses, 
+												y, m, viruses_per_month, regions))
+		if self.outgroup is not None:
+			self.filtered_viruses.append(self.outgroup)
+			print len(filtered_viruses), "with outgroup"
+		self.virus_subsample = filtered_viruses
+
+	def viruses_by_date_region(self, viruses):
+		'''
+		make dictionary of lists of viruses belonging to a certain date and region
+		'''
+		from collections import defaultdict
+		virus_tuples = defaultdict(list)
+		for v in viruses:
+			vdate = datetime.datetime.strptime(v['date'], '%Y-%m-%d').date()
+			virus_tuples[(vdate.year, vdate.month, v['region'])].append(v)
+
+		return virus_tuples
+
+	def select_viruses(priority_viruses,other_viruses, y, m, viruses_per_month, regions):
+		'''
+		select viruses_per_month strains as evenly as possible from all regions
+		'''
+		from itertools import izip_longest
+		select_set = []
+		for vset in [priority_viruses, other_viruses]:
+			for representative in izip_longest(*[vset[(y,m,r)] for r in regions], fillvalue = None):
+				select_set.extend([v in representative if v is not None])
+		return select_set[:viruses_per_month]
+
+
+class flu_filter(virus_filter):
+
+	def __init__(self,fasta_fname):
+		self.fasta_header = {0:'strain', 1:'accession', 3:'passage', 5:'date' }
+		self.viruses = self.parse_gisaid(fasta_fname)
+		self.fix_strain_names()
+		self.vaccine_strains=[]
+
+	def parse_gisaid(self, fasta):
+		"""Parse FASTA file from GISAID with default header formating"""
+		viruses = []
+		try:
+			handle = open(fasta, 'r')
+		except IOError:
+			print fasta + " not found"
+		else:
+			for record in SeqIO.parse(handle, "fasta"):
+				words = record.description.replace(">","").replace(" ","").split('|')
+				v = {key:words[ii] for ii, key in self.fasta_header.iteritems()}
+				v['db']="GISAID",
+				v['seq']=str(record.seq).upper()
+				if passage != "":
+					v['passage'] = passage
+				viruses.append(v)
+			handle.close()
+		return viruses
+
+	def fix_strain_names(self):
+		for v in self.viruses:
+			v['strain'] = v['strain'].replace('\'','').replace('(','').replace(')','').replace('H3N2','').replace('Human','').replace('human','').replace('//','/')
+
+	def filter_passage(self):
+		self.viruses = filter(lambda v: re.match(r'^E\d+', v.get('passage',''), re.I) == None, self.viruses)
+		self.viruses = filter(lambda v: re.match(r'^Egg', v.get('passage',''), re.I) == None, self.viruses)
+
+	def filter(self):
+		self.filter_generic(prepend_strains = self.vaccine_strains)	
+		self.filter_passage()
+		print len(self.viruses) + " without egg passage"
+		self.filter_geo()
+
+	def self.filter_geo(self):
+		"""Label viruses with geographic location based on strain name"""
+		"""Location is to the level of country of administrative division when available"""
+		reader = csv.DictReader(open("source-data/geo_synonyms.tsv"), delimiter='\t')		# list of dicts
+		label_to_country = {}
+		for line in reader:
+			label_to_country[line['label'].lower()] = line['country']
+		for v in self.viruses:
+			label = re.match(r'^A/([^/]+)/', v['strain']).group(1).lower()					# check first for whole geo match
+			v['country'] = 'Unknown'
 			if label in label_to_country:
 				v['country'] = label_to_country[label]
+			else:
+				label = re.match(r'^A/([^\-^\/]+)[\-\/]', v['strain']).group(1).lower()		# check for partial geo match
+				if label in label_to_country:
+					v['country'] = label_to_country[label]
 
-	reader = csv.DictReader(open("source-data/geo_regions.tsv"), delimiter='\t')		# list of dicts
-	country_to_region = {}
-	for line in reader:
-		country_to_region[line['country']] = line['region']
-	for v in viruses:
-		v['region'] = 'Unknown'
-		if v['country'] in country_to_region:
-			v['region'] = country_to_region[v['country']]
-	
-	return filter(lambda v: v['region'] != 'Unknown', viruses)
+		reader = csv.DictReader(open("source-data/geo_regions.tsv"), delimiter='\t')		# list of dicts
+		country_to_region = {}
+		for line in reader:
+			country_to_region[line['country']] = line['region']
+		for v in self.viruses:
+			v['region'] = 'Unknown'
+			if v['country'] in country_to_region:
+				v['region'] = country_to_region[v['country']]
+		
+		self.viruses = filter(lambda v: v['region'] != 'Unknown', self.viruses)
 
-def get_virus_tuples(viruses):
-	'''
-	make dictionary of lists of viruses belonging to a certain date and region
-	'''
-	from collections import defaultdict
-	virus_tuples = defaultdict(list)
-	for v in viruses:
-		vdate = datetime.datetime.strptime(v['date'], '%Y-%m-%d').date()
-		virus_tuples[(vdate.year, vdate.month, v['region'])].append(v)
-
-	return virus_tuples
-
-def streamline(viruses, years_back, viruses_per_month):
-	"""Subsample x viruses per month"""
-	"""Take from beginning of list - this will prefer longer sequences"""
-	"""Take viruses 1 per region in a cycle to get geographic diversity"""
-	"""But pad with additional viruses from populous regions if necessary"""
-
-	virus_tuples = get_virus_tuples(viruses)
-
-	filtered_viruses = []
-	first_year = datetime.datetime.today().year - years_back
-	first_month = datetime.datetime.today().month
-	regions = [v['region'] for v in viruses]
-	regions = list(set(regions))
-
-	print "Filtering between " + str(first_month) + "/" + str(first_year) + " and today"
-	print "Selecting " + str(viruses_per_month) + " viruses per month"
-	y = first_year
-	for m in range(first_month,13):
-		filtered_viruses.extend(select_viruses(virus_tuples, y, m, viruses_per_month, regions))
-	for y in range(first_year+1,datetime.datetime.today().year+1):
-		for m in range(1,13):
-			filtered_viruses.extend(select_viruses(virus_tuples, y, m, viruses_per_month, regions))
-	return filtered_viruses
-
-def select_viruses(virus_tuples, y, m, viruses_per_month, regions):
-	'''
-	select viruses_per_month strains as evenly as possible from all regions
-	'''
-	from itertools import izip_longest
-	select_set = []
-	for representative in izip_longest(*[virus_tuples[(y,m,r)] for r in regions], fillvalue = None):
-		select_set.extend([v in representative if v is not None])
-	return select_set[:viruses_per_month]
-
-
-def main(in_fname=None, years_back=3, viruses_per_month=50):
-
-	print "--- Filter at " + time.strftime("%H:%M:%S") + " ---"
-
-	if in_fname is None: in_fname = 'data/gisaid_epiflu_sequence.fasta'
-	viruses = parse_gisaid(in_fname)
-	print str(len(viruses)) + " initial viruses"
-
-	# sort by sequence length
-	viruses = sort_length(viruses)
-
-	# fix strain names
-	fix_strain_names(viruses)
-
-	# add vaccine strains
-	viruses = vaccine_strains + viruses
-	print str(len(viruses)) + " with vaccine strains"
-
-	# filter strain names
-	viruses = filter_strain_names(viruses)
-	print str(len(viruses)) + " with proper strain names"
-
-	# filter short sequences
-	viruses = filter_length(viruses)
-	print str(len(viruses)) + " without truncation"
-
-	# filter imprecise dates
-	viruses = filter_date(viruses)
-	print str(len(viruses)) + " with precise dates"
-
-	# filter passage history
-	viruses = filter_passage(viruses)
-	print str(len(viruses)) + " without egg passage"
-
-	# filter to unique strains
-	viruses = filter_unique(viruses)
-	print str(len(viruses)) + " with unique strain names"
-	
-	# append geo information
-	viruses = append_country_and_region(viruses)
-	print str(len(viruses)) + " with geographic information"	
-
-	# reduce to manageable volume
-	viruses = streamline(viruses, years_back, viruses_per_month)
-	print str(len(viruses)) + " after streamlining"
-
-	# add outgroup
-	add_outgroup(viruses)
-	print str(len(viruses)) + " with outgroup"
-	out_fname = 'data/virus_filter.json'
-	write_json(viruses, out_fname)
-	return out_fname
-	
-if __name__ == "__main__":
-	main()
