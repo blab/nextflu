764eae82d77c9d6b9222915d57a2f3c1ddfb0c56
Richard Neher <richard@ag-neher-xps13.(none)>
2015-02-26 23:51:53 +0100

    added new processing pipeline

---
+++ augur/nextflu_config.py
@@ -0,0 +1,6 @@
+config = {
+	'virus':'H3N2',
+	'alignment_file':'data/20150222_all_H3N2_HA1.fasta',
+	'fasta_fields':{0:'strain', 1:"date", 4:"passage", -1:'accession'},
+	'outgroup':'A/Beijing/32/1992'
+}
