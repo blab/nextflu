895c3b309fefd228e37c05fdf504730deb77477b
Richard Neher <richard@ag-neher-xps13.(none)>
2015-03-04 18:21:49 +0100

    fixed genotype parsing to now look for fields like 159F/225D in the frequencies.genotype json

--- auspice/js/auspice.js
+++ auspice/js/auspice.js
@@ -1060,8 +1060,14 @@ d3.json("data/frequencies.json", function(error, json){
 									if (tmp[i].length>0) positions.push(tmp[i]);
 								}
 							}
-							if (typeof region == "undefined") region="global"; 
-							return [region, positions];});
+							if (typeof region == "undefined") region="global";
+							// sort of this is a multi mutation genotype
+							if (positions.length>1){
+								positions.sort(function (a,b){
+									return parseInt(a.substring(0,a.length-1)) - parseInt(b.substring(0,b.length-1));
+								});
+							}
+							return [region, positions.join('/')];});
 		return mutations;
 	};
 
@@ -1070,20 +1076,22 @@ d3.json("data/frequencies.json", function(error, json){
 	of the genotype matches at the specified positions
 	**/
 	function get_frequencies(region, gt){
-		console.log("calculating frequencies for :"+gt);
 		var freq = [];
 		for (var pi=0; pi<pivots.length; pi++){freq[freq.length]=0;}
-		if (json["genotypes"][region][gt[0]]!=undefined) {
+		if (json["genotypes"][region][gt]!=undefined) {
+			console.log(gt+" found as genotype");
 			for (var pi=0; pi<freq.length; pi++){
-				freq[pi]+=json["genotypes"][region][gt[0]][pi];
+				freq[pi]+=json["genotypes"][region][gt][pi];
 			}
-		}else if (json["mutations"][region][gt[0]]!=undefined) {
+		}else if (json["mutations"][region][gt]!=undefined) {
+			console.log(gt+" found as mutation");
 			for (var pi=0; pi<freq.length; pi++){
-				freq[pi]+=json["mutations"][region][gt[0]][pi];
+				freq[pi]+=json["mutations"][region][gt][pi];
 			}
-		}else if (json["clades"][region][gt[0].toLowerCase()]!=undefined) {
+		}else if (json["clades"][region][gt.toLowerCase()]!=undefined) {
+			console.log(gt+" found as clade");
 			for (var pi=0; pi<freq.length; pi++){
-				freq[pi]+=json["clades"][region][gt[0].toLowerCase()][pi];
+				freq[pi]+=json["clades"][region][gt.toLowerCase()][pi];
 			}
 		}
 		return freq.map(function (d) {return Math.round(d*100)/100;});
