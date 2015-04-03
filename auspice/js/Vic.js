14025804c14fe30c687d756dfc854e9e23705b6d
Richard Neher <richard@ag-neher-xps13.(none)>
2015-04-03 20:27:35 +0200

    added vaccine strains to B-flu js

--- auspice/js/Vic.js
+++ auspice/js/Vic.js
@@ -2,7 +2,10 @@ var file_prefix = 'Vic_';
 var dfreq_dn = 2;
 
 var	vaccineChoice = {};
-//vaccineChoice['A/California/07/2009'] = "2009-09-25";
+vaccineChoice['B/Shangdong/7/97'] = "1999-09-25";
+vaccineChoice['B/HongKong/330/2001'] = "2002-09-25";
+vaccineChoice['B/Malaysia/2506/2004'] = "2006-09-25";
+vaccineChoice['B/Brisbane/60/2008'] = "2009-09-25";
 
 var epiColorDomain = [0,1,2,3, 4,5,6,7,8,9,10];
 var nonEpiColorDomain = [0,1,2,3,4,5,6,7,8,9,10,11];
