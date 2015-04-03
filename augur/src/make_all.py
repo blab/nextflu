476d37904bd171b1f5c9e503beb5f392eca9accb
Richard <richard.neher@tuebingen.mpg.de>
2015-04-03 18:36:13 +0200

    added shebang and extra binary variable in case I run it on the tuebingen cluster.

--- augur/src/make_all.py
+++ augur/src/make_all.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import subprocess,glob, os
 
 run_pipeline = []
@@ -8,24 +9,26 @@ for flu in ["H3N2", "H1N1pdm", "H1N1", "Vic", "Yam"]:
 		if len(auspice_files)==0 or min([os.path.getmtime(fname) for fname in auspice_files])<max([os.path.getmtime(fname) for fname in input_files]):
 			run_pipeline.append(flu)
 
+#python_binary = '/ebio/ag-neher/share/programs/bin/python'
+python_binary = 'python'
 
 if 'H3N2' in run_pipeline:
-	call = map(str, ['python', 'src/H3N2_process.py', '-v', 30, '-y', 3, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/H3N2_'])
+	call = map(str, [python_binary, 'src/H3N2_process.py', '-v', 50, '-y', 3, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/H3N2_'])
 	print call
 	subprocess.call(call)
 if 'H1N1' in run_pipeline:
-	call = map(str, ['python', 'src/H1N1historical_process.py', '-v', 20, '--interval', 1990, 2010, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/H1N1_'])
+	call = map(str, [python_binary, 'src/H1N1historical_process.py', '-v', 20, '--interval', 1990, 2010, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/H1N1_'])
 	print call
 	subprocess.call(call)
 if 'H1N1pdm' in run_pipeline:
-	call = map(str, ['python', 'src/H1N1pdm_process.py', '-v', 30, '-v', 6, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/H1N1pdm_'])
+	call = map(str, [python_binary, 'src/H1N1pdm_process.py', '-v', 30, '-y', 6, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/H1N1pdm_'])
 	print call
 	subprocess.call(call)
 if 'Vic' in run_pipeline:
-	call = map(str, ['python', 'src/Vic_process.py', '-v', 30, '-v', 6, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/Vic_'])
+	call = map(str, [python_binary, 'src/Vic_process.py', '-v', 30, '-y', 6, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/Vic_'])
 	print call
 	subprocess.call(call)
 if 'Yam' in run_pipeline:
-	call = map(str, ['python', 'src/Vic_process.py', '-v', 30, '-v', 6, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/Yam_'])
+	call = map(str, [python_binary, 'src/Vic_process.py', '-v', 30, '-y', 6, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/Yam_'])
 	print call
 	subprocess.call(call)
