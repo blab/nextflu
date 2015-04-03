d77f9e4cb9ccb82cf8e36c7b6cf014683ee8580e
Richard Neher <richard@ag-neher-xps13.(none)>
2015-04-03 12:25:32 +0200

    added buttons for B, added files for Vic and Yam

---
+++ auspice/js/Vic.js
@@ -0,0 +1,9 @@
+var file_prefix = 'BVic_';
+var dfreq_dn = 2;
+
+var	vaccineChoice = {};
+//vaccineChoice['A/California/07/2009'] = "2009-09-25";
+
+var epiColorDomain = [0,1,2,3, 4,5,6,7,8,9,10];
+var nonEpiColorDomain = [0,1,2,3,4,5,6,7,8,9,10,11];
+var rbsColorDomain = [0,0.5, 1,1.5, 2];
