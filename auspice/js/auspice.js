cd29c09ad732d1abc7b52e16ddd6536df44e0457
Trevor Bedford <trvrb@trevorbedford.com>
2015-04-02 19:25:30 -0700

    Probably a better pattern for JS. First load H3N2-specific parameters then load auspice.

--- auspice/js/auspice.js
+++ auspice/js/auspice.js
@@ -21,16 +21,7 @@ function gatherInternals(node, internals) {
 }
 
 function getVaccines(tips) {
-	vaccineChoice = {};
-	vaccineChoice['A/Fujian/411/2002'] = "2003-09-25";
-	vaccineChoice['A/California/7/2004'] = "2005-02-21";
-	vaccineChoice['A/Wisconsin/67/2005'] = "2006-02-21";
-	vaccineChoice['A/Brisbane/10/2007'] = "2007-09-25";
-	vaccineChoice['A/Perth/16/2009'] = "2009-09-25";
-	vaccineChoice['A/Victoria/361/2011'] = "2012-02-21";
-	vaccineChoice['A/Texas/50/2012'] = "2013-09-25";
-	vaccineChoice['A/Switzerland/9715293/2013'] = "2014-09-25";
-	vaccineStrains = Object.keys(vaccineChoice);
+	var vaccineStrains = Object.keys(vaccineChoice);
 	vaccines = [];
 	tips.forEach(function (tip) {
 		if (vaccineStrains.indexOf(tip.strain) != -1) {
