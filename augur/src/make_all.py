7256910fcebb79f757aac974126852b5d0947948
Richard Neher <richard@ag-neher-xps13.(none)>
2015-04-04 12:33:44 +0200

    generalized make_all to allow for the HA1 option

--- augur/src/make_all.py
+++ augur/src/make_all.py
@@ -56,28 +56,37 @@ if __name__=="__main__":
 	parser = argparse.ArgumentParser(description = "ammend existing files with downloaded viruses, rerun")
 	parser.add_argument('--infile', type = str, default = "gisaid_epiflu_sequence.fasta")
 	parser.add_argument('--bin', type = str, default = "python")
+	parser.add_argument('--HA1', action = "store_true", default = False)
+	parser.add_argument('--all', action = "store_true", default = False)
+	parser.add_argument('-r', type = float, default = 1.0)
 	params = parser.parse_args()
 
-	run_pipeline = ammend_files(params.infile, lineages = ['H3N2', 'H1N1pdm', 'Vic', 'Yam'], threshold = 10, directory = 'data/')
-	print run_pipeline
+	if params.all:	
+		run_pipeline = ammend_files(params.infile, lineages = ['H3N2', 'H1N1pdm', 'Vic', 'Yam'], threshold = 1, directory = 'data/')
+		run_pipeline = ['H3N2', 'H1N1pdm', 'Vic', 'Yam']
+	else:
+		run_pipeline = ammend_files(params.infile, lineages = ['H3N2', 'H1N1pdm', 'Vic', 'Yam'], threshold = 10, directory = 'data/')
+
+	common_args = ['--skip', 'genotype_frequencies','-r', params.r]
+	if params.HA1: common_args.append('--HA1')
 
 	if 'H3N2' in run_pipeline:
-		call = map(str, [params.bin, 'src/H3N2_process.py', '-v', 50, '-y', 3, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/H3N2_'])
+		call = map(str, [params.bin, 'src/H3N2_process.py', '-v', 50, '-y', 3,  '--prefix', 'data/H3N2_'])
 		print call
 		subprocess.call(call)
 	if 'H1N1' in run_pipeline:
-		call = map(str, [params.bin, 'src/H1N1historical_process.py', '-v', 20, '--interval', 1990, 2010, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/H1N1_'])
+		call = map(str, [params.bin, 'src/H1N1historical_process.py', '-v', 20, '--interval', 1990, 2010, '--prefix', 'data/H1N1_'])
 		print call
 		subprocess.call(call)
 	if 'H1N1pdm' in run_pipeline:
-		call = map(str, [params.bin, 'src/H1N1pdm_process.py', '-v', 30, '-y', 6, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/H1N1pdm_'])
+		call = map(str, [params.bin, 'src/H1N1pdm_process.py', '-v', 30, '-y', 6, '--prefix', 'data/H1N1pdm_'])
 		print call
 		subprocess.call(call)
 	if 'Vic' in run_pipeline:
-		call = map(str, [params.bin, 'src/Vic_process.py', '-v', 30, '-y', 6, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/Vic_'])
+		call = map(str, [params.bin, 'src/Vic_process.py', '-v', 30, '-y', 6, '--prefix', 'data/Vic_'])
 		print call
 		subprocess.call(call)
 	if 'Yam' in run_pipeline:
-		call = map(str, [params.bin, 'src/Yam_process.py', '-v', 30, '-y', 6, '--skip', 'genotype_frequencies', '-r', 1.0, '--prefix', 'data/Yam_'])
+		call = map(str, [params.bin, 'src/Yam_process.py', '-v', 30, '-y', 6, '--prefix', 'data/Yam_'])
 		print call
 		subprocess.call(call)
