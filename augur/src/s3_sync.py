9ab15420ef31cf1e3a55bba1cfcd46d6d0c92817
Trevor Bedford <trvrb@trevorbedford.com>
2015-04-10 22:00:21 -0700

    Rename production.py to s3_sync.py. Allow pushes to dev.

--- augur/src/s3_sync.py
+++ augur/src/s3_sync.py
@@ -4,7 +4,7 @@ import argparse
 verbose = False
 lineages = ['H3N2', 'H1N1pdm', 'Vic', 'Yam']
 
-def pull_from_dev(lineage, directory = '../auspice/data/', bucket = 'nextflu-dev'):
+def pull(lineage, directory = '../auspice/data/', bucket = 'nextflu-dev'):
 	"""Retrieve JSON files from S3 dev bucket"""
 	"""Boto expects environmental variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY"""
 	directory = directory.rstrip('/')+'/'
@@ -21,7 +21,7 @@ def pull_from_dev(lineage, directory = '../auspice/data/', bucket = 'nextflu-dev
 		k.get_contents_to_filename(directory+json)
 		print json,"retrieved"
 	
-def push_to_production(lineage, directory = '../auspice/data/', bucket = 'nextflu', cloudfront = 'E1ZD3XPSGXABBI'):
+def push(lineage, directory = '../auspice/data/', bucket = 'nextflu-dev', cloudfront = 'E1XKGZG0ZTX4YN'):
 	"""Upload JSON files to S3 production bucket"""
 	"""Boto expects environmental variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY"""
 	directory = directory.rstrip('/')+'/'
@@ -47,15 +47,23 @@ def push_to_production(lineage, directory = '../auspice/data/', bucket = 'nextfl
 if __name__=="__main__":
 	parser = argparse.ArgumentParser(description = "sync JSONs between local storage, dev server and production server")	
 	parser.add_argument('--dev_bucket', type = str, default = "nextflu-dev", help = "dev bucket for JSON files")
-	parser.add_argument('--live_bucket', type = str, default = "nextflu", help = "dev bucket for JSON files")
-	parser.add_argument('--pull', action = "store_true", default = False, help = "pull local files from dev")
-	parser.add_argument('--push', action = "store_true", default = False, help = "push local files to production")
+	parser.add_argument('--pro_bucket', type = str, default = "nextflu", help = "production bucket for JSON files")
+	parser.add_argument('--dev_cloudfront', type = str, default = "E1XKGZG0ZTX4YN", help = "dev cloudfront distribution")
+	parser.add_argument('--pro_cloudfront', type = str, default = "E1ZD3XPSGXABBI", help = "production cloudfront distribution")	
+	parser.add_argument('--pull_dev', action = "store_true", default = False, help = "pull local files from dev")
+	parser.add_argument('--push_dev', action = "store_true", default = False, help = "push local files to dev")
+	parser.add_argument('--pull_pro', action = "store_true", default = False, help = "pull local files from pro")
+	parser.add_argument('--push_pro', action = "store_true", default = False, help = "push local files to pro")	
 	parser.add_argument('--sync', action = "store_true", default = False, help = "sync dev to production")	
 	params = parser.parse_args()
 
 	for lineage in lineages:
 		print '\nLineage',lineage
-		if params.pull or params.sync:
-			pull_from_dev(lineage, directory = '../auspice/data/', bucket = params.dev_bucket)
-		if params.push or params.sync:
-			push_to_production(lineage, directory = '../auspice/data/', bucket = params.live_bucket)			
+		if params.pull_dev or params.sync:
+			pull(lineage, directory = '../auspice/data/', bucket = params.dev_bucket)
+		if params.push_dev:
+			push(lineage, directory = '../auspice/data/', bucket = params.dev_bucket, cloudfront = params.dev_cloudfront)
+		if params.pull_pro:
+			pull(lineage, directory = '../auspice/data/', bucket = params.pro_bucket)
+		if params.push_pro or params.sync:
+			push(lineage, directory = '../auspice/data/', bucket = params.pro_bucket, cloudfront = params.pro_cloudfront)						
