61aa93e8f9cf68dbf8d05f7fc0f65809df95d645
Trevor Bedford <trvrb@trevorbedford.com>
2015-04-14 17:55:28 -0700

    Include auspice.js template file.

---
+++ auspice/js/auspice.js
@@ -0,0 +1,15 @@
+---
+---
+
+{% include_relative d3.min.js %}
+{% include_relative d3.tip.js %}
+{% include_relative c3.min.js %}
+{% include_relative autocomplete.js %}
+{% include_relative globals.js %}
+{% include_relative date.js %}
+{% include_relative legend.js %}
+{% include_relative colors.js %}
+{% include_relative tree_functions.js %}
+{% include_relative tooltips.js %}
+{% include_relative tree.js %}
+{% include_relative frequencies.js %}
