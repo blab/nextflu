871d74b12a08afef3a4732f23d1377d7a803dc06
Richard Neher <richard@ag-neher-xps13.(none)>
2015-05-10 10:42:53 +0200

    made dfreqColorScale more maintainable

--- auspice/js/H3N2.js
+++ auspice/js/H3N2.js
@@ -17,7 +17,7 @@ var epiColorDomain = [4,5,6,7,8,9,10,11,12,13];
 var nonEpiColorDomain = [2,3,4,5,6,7,8,9,10,11];
 var rbsColorDomain = [0,1,2,3,4];
 var dateDomain = genericDomain;
-var dfreqColorDomain = [-0.18, -0.14, -0.10, -0.06, -0.02, 0.02, 0.06, 0.10, 0.14, 0.18];
+var dfreqColorDomain = genericDomain.map(function(d){return Math.round(100*(-0.18+d*0.36))/100;});
 
 var time_ticks = [2012.5, 2013, 2013.5, 2014, 2014.5, 2015];
 var	time_window = 1.0;  // layer of one year that is considered current or active
