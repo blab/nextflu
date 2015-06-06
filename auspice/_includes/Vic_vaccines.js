30b1bac10c26e9e13d62f263223a680dcc9402f2
Richard <richard.neher@tuebingen.mpg.de>
2015-06-06 16:22:49 +0200

    fixed numbering for H1 and B

--- auspice/_includes/Vic_vaccines.js
+++ auspice/_includes/Vic_vaccines.js
@@ -5,5 +5,5 @@ vaccineChoice['B/Malaysia/2506/2004'] = "2006-09-25";
 vaccineChoice['B/Brisbane/60/2008'] = "2009-09-25";
 var vaccineStrains = Object.keys(vaccineChoice);
 
-var genome_annotation = {'HA1':[[1,1,1], [1,160,329]],
-                         'HA2':[[1.2,1.2,1.2], [329,450,580]]}
+var genome_annotation = {'HA1':[[1,1,1], [1,160,346]],
+                         'HA2':[[1.2,1.2,1.2], [346,450,600]]}
