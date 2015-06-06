5be5539485d513171e52f6fcb17cecbab0f5a352
Richard <richard.neher@tuebingen.mpg.de>
2015-06-06 16:10:42 +0200

    improved spacing

--- auspice/js/frequencies.js
+++ auspice/js/frequencies.js
@@ -207,12 +207,17 @@ d3.json(path + file_prefix + "frequencies.json", function(error, json){
 
 	var chart_types = {'':'bar'}
 	var chart_xaxis = {'':'x'}
+	var ymin = 0;
 	for (x in genome_annotation){
 		chart_data['x'+x] = genome_annotation[x][1];
 		chart_data[x] = genome_annotation[x][0].map(function(d) {return -0.1*d;});
+		if (ymin>chart_data[x][0]){
+			ymin = chart_data[x][0];
+		}
 		chart_types[x] = 'line';
 		chart_xaxis[x] = 'x'+x;
 	}
+	ymin-=0.08;
 	console.log(chart_data);
 	console.log(chart_types);
 	console.log(chart_xaxis);
@@ -236,6 +241,7 @@ d3.json(path + file_prefix + "frequencies.json", function(error, json){
 					values: [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6],
 					outer: false
 				},
+				min:ymin,
 			},
 			x: {
 				label: {
@@ -246,7 +252,7 @@ d3.json(path + file_prefix + "frequencies.json", function(error, json){
 					outer: false,
 					values: [100,200,300,400,500]				
 				}
-			}
+			},
 		},			
 		data: {
 			xs: chart_xaxis,
