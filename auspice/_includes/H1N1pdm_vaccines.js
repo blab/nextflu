3248b495c8bf93ee4584e9508e309c58a7a7ce2a
Richard <richard.neher@tuebingen.mpg.de>
2015-06-06 16:11:28 +0200

    added annotations to other flus

--- auspice/_includes/H1N1pdm_vaccines.js
+++ auspice/_includes/H1N1pdm_vaccines.js
@@ -1,3 +1,6 @@
 var	vaccineChoice = {};
 vaccineChoice['A/California/07/2009'] = "2009-09-25";
 var vaccineStrains = Object.keys(vaccineChoice);
+
+var genome_annotation = {'HA1':[[1,1,1], [1,160,329]],
+                         'HA2':[[1.2,1.2,1.2], [329,450,580]]}
