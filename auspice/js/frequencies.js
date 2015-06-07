aa10df05ccbff5b87e8819ebc00221e327fb6343
Trevor Bedford <trvrb@trevorbedford.com>
2015-06-07 12:05:22 -0700

    Work when genome_annotation is absent.

--- auspice/js/frequencies.js
+++ auspice/js/frequencies.js
@@ -205,19 +205,21 @@ d3.json(path + file_prefix + "frequencies.json", function(error, json){
 		}
 	}
 
-	var ymin=0;
 	var chart_types = {'':'bar'}
 	var chart_xaxis = {'':'x'}
-	for (x in genome_annotation){
-		chart_data['x'+x] = genome_annotation[x][1];
-		chart_data[x] = genome_annotation[x][0].map(function(d) {return -0.05*d;});
-		if (ymin>chart_data[x][0]){
-			ymin = chart_data[x][0];
+	var ymin = 0;
+	if (typeof genome_annotation !== 'undefined') {
+		for (x in genome_annotation){
+			chart_data['x'+x] = genome_annotation[x][1];
+			chart_data[x] = genome_annotation[x][0].map(function(d) {return -0.1*d;});
+			if (ymin>chart_data[x][0]){
+				ymin = chart_data[x][0];
+			}
+			chart_types[x] = 'line';
+			chart_xaxis[x] = 'x'+x;
 		}
-		chart_types[x] = 'line';
-		chart_xaxis[x] = 'x'+x;
+		ymin-=0.04;
 	}
-	ymin-=0.04;
 	console.log(chart_data);
 	console.log(chart_types);
 	console.log(chart_xaxis);
@@ -238,7 +240,7 @@ d3.json(path + file_prefix + "frequencies.json", function(error, json){
 					position: 'outer-middle'	
 				},
 				tick: {
-					values: [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6],
+					values: [0.0, 0.2, 0.4, 0.6],
 					outer: false
 				},
 				min:ymin,
