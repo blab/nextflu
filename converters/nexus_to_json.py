c6079596e031112aa8ec0d0045a872d259c3a6ab
Andrew Rambaut <rambaut@gmail.com>
2015-06-14 11:27:45 +0100

    Generalized nexus_to_json
    Added field order specification (needs command line options). Writes
    branch rates and substitution xvalues.

--- converters/nexus_to_json.py
+++ converters/nexus_to_json.py
@@ -21,24 +21,35 @@ def toYearFraction(date):
 
     return date.year + fraction
 
-def parse_taxon_fields(taxon, node):
+def parse_taxon_fields(taxon, node, field_names):
     fields = taxon.__str__().split("|")
     
-    node["strain"] = fields[2]
-    node["lab"] = fields[3]
-    node["country"] = fields[4]
-    node["region"] = fields[5]
-    
-    date = parse(fields[8])
+    index = 0
+
+    for field in field_names:
+        if len(field) > 0:
+            node[field] = fields[index]
+        index += 1
+
+    date = parse(fields[-1])
     node["date"] = date.strftime('%Y-%m-%d')
     node["datevalue"] = toYearFraction(date)
 
-def process_node(node, x, y):    
+def process_node(node, x_time, x_subst, y, field_order):    
     y0 = 0.0
-    
+
+    rate = node.annotations.get_value("rate")
+    if rate is not None:
+        rate = float(rate)
+
+    if node.edge_length is not None:
+        x_time += node.edge_length
+        x_subst += node.edge_length * rate
+
+
     node1 = {}
     if node.is_leaf():
-        parse_taxon_fields(node.taxon, node1)
+        parse_taxon_fields(node.taxon, node1, field_order)
         node1['name'] = node.taxon.__str__()
         y0 = y[0]
         y[0] += 1
@@ -46,15 +57,18 @@ def process_node(node, x, y):
         node1['clade'] = 0;
         node1['children'] = [];
         for child in node.child_nodes():
-            x1 = x + child.edge_length
-            child1, y1 = process_node(child, x1, y)
+            child1, y1 = process_node(child, x_time, x_subst, y)
             y0 += y1;
             node1['children'].append(child1)
             
         y0 /= len(node.child_nodes())
         
-    node1['xvalue'] = x
+    node1['xvalue'] = x_time
+    node1['xvalue_subst'] = x_subst
     node1['yvalue'] = y0
+    
+    if rate is not None:
+        node1['rate'] = rate
     
     return node1, y0
 
@@ -70,8 +84,13 @@ def main():
 		schema='nexus',
 		rooting="default-rooted")
  
+ 	#todo - specify these from the command line
+ 	#field_order = ["virus","strain","lab","country","region"] # MERS-CoV
+	field_order = ["virus","lab","strain","accession","country","region"] # EBOV
+ 
 	y = [0]
-	root, y1 = process_node(tree.seed_node, 0.0, y)
+	root, y1 = process_node(tree.seed_node, 0.0, 0.0, y, field_order)
+    
 
 	with open(args.args[1], 'w') as outfile:
 		json.dump(root, outfile, indent=4)
