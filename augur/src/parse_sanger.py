47df074eb9fab01e86d21ba8e02baf670ecd0b84
Richard <richard.neher@tuebingen.mpg.de>
2015-06-30 10:40:17 +0200

    improved sanger parsing script

--- augur/src/parse_sanger.py
+++ augur/src/parse_sanger.py
@@ -1,15 +1,21 @@
 from Bio import SeqIO
 
-infname = 'data/ig_ebo_145_27Jun15.fas'
+infname = 'data/ig_ebo_145_29Jun15_correct.fas'
 informat = {'lab':0, 'strain':1, 'country':2, 'region':3, 'date':4}
 outformat = ['virus', 'accession', 'strain', 'lab', 'country', 'region','location','smth', 'date']
 outfname = 'data/sanger_27Jun15.fas'
+existing_file_name = 'data/ebola_908.fasta'
 
 generic = {'virus':'EBOV', 'accession':'', 'lab':'', 'country':'', 'region':'', 'location':'', 'smth':'' }
 
+existing = []
+for seq in SeqIO.parse(existing_file_name, 'fasta'):
+	existing.append(seq.name.split('|')[outformat.index('strain')].upper())
+
+skipped=0
+written=0
 with open(outfname, 'w') as outfile:
 	for seq in SeqIO.parse(infname, 'fasta'):
-		print seq.name
 		fields = seq.name.rstrip('*').split('|')
 		params = {key:fields[informat[key]] for key in informat}
 		new_name = '|'.join([params[key] if key in params else generic[key]
@@ -18,4 +24,13 @@ with open(outfname, 'w') as outfile:
 		seq.name=new_name
 		seq.id=new_name
 		seq.description=''
-		SeqIO.write(seq, outfile, 'fasta')
+		if fields[informat['strain']].upper() in existing:
+			print 'skipped:',seq.name
+			skipped+=1
+		else:
+			print 'written:',seq.name			
+			SeqIO.write(seq, outfile, 'fasta')
+			written+=1
+
+print "written",written,"new sequences"
+print "skipped",skipped,"sequences"
