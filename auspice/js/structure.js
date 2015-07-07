6fc713b1de339ed07248efcaef5661ea58956b00
Richard Neher <richard.neher@tuebingen.mpg.de>
2015-07-07 08:45:35 +0200

    removed buttons

--- auspice/js/structure.js
+++ auspice/js/structure.js
@@ -4,7 +4,7 @@ function make_structure(){
 	console.log('drawing structure');
 	var jsmolscript =  "load /data/5HMG.pdb; cpk off; wireframe off; trace;zoom on;"
 					   +"zoom 115;set showhydrogens off; color background white;" 
-					   +" select ligand; trace off; spin on; set frank off; "
+					   +" select ligand; trace off; spin off; set frank off; "
 					   +"set echo bottom left; color echo gray; font echo 14 arial;"
 					   +structure_HI_mutations;
 //					   +"spacefill 200;color orange;";
@@ -23,11 +23,5 @@ function make_structure(){
 		script:	jsmolscript}
 
 	myapplett = $("#HA_struct").html(Jmol.getAppletHtml("jmolApplet0",Info));
-
-	$("#HA_buttons").html(
-		Jmol.jmolButton(jmolApplet0, "spin on","Spin ON")
-		 +Jmol.jmolButton(jmolApplet0, "spin off","Spin OFF")
-		 +Jmol.jmolButton(jmolApplet0, "write PNGJ flusurver.png","Save IMAGE")
-	 );
 }
 
