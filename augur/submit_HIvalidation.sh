d1d586bc822d19866591fea5413b87ed10dbaad6
Richard <richard.neher@tuebingen.mpg.de>
2015-07-23 14:39:51 +0200

    small change in validation

--- augur/submit_HIvalidation.sh
+++ augur/submit_HIvalidation.sh
@@ -1,6 +1,5 @@
+#!/usr/bin/env bash
 
-    #!/usr/bin/env bash
-
-echo src/HI_map_validation.py --prefix $1_ --flutype $1 --resolution $2 --min_aamuts $3 --reg $5 $4 $6 
-/ebio/ag-neher/share/programs/bin/python src/HI_map_validation.py --prefix $1_ --flutype $1 --resolution $2 --min_aamuts $3 --reg $5 $4 $6
+echo src/HI_map_validation.py --prefix $1_ --flutype $1 --resolution $2 --min_aamuts $3 --reg $4 $5 $6 
+/ebio/ag-neher/share/programs/bin/python src/HI_map_validation.py --prefix $1_ --flutype $1 --resolution $2 --min_aamuts $3 --reg $4 $5 $6
 
--- augur/submit_all_HImap.py
+++ augur/submit_all_HImap.py
@@ -3,11 +3,11 @@ import os
 
 res='12y'
 for reg in [0.1, 0.3, 1, 3, 10]:
-    for flu in ['H3N2', 'H1N1pdm', 'Vic', 'Yam']:
+    for flu in ['H3N2', 'Vic', 'Yam']:
         for minaa in [0]: #[0,1,'epi']:
             for model in ["--map_to_tree",""]:
                 for train_strains in ['', '--train_strains']:
-                    call = ' '.join(['qsub -cwd -l h_vmem=8G -l h_rt=20:59:00 submit_HIvalidation.sh',
-                              flu, res, str(minaa), train_strains, str(reg), model])
+                    call = ' '.join(['qsub -cwd -l h_vmem=8G -l h_rt=00:59:00 submit_HIvalidation.sh',
+                              flu, res, str(minaa), str(reg), train_strains,  model])
                     print call
                     os.system(call)
