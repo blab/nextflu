bf6b7a2a732d0e542adb17dd7aa4f0baeaaf7001
Trevor Bedford <trvrb@trevorbedford.com>
2015-09-16 20:48:13 -0700

    Adjust frequencies to be more stable, i.e. less follow through in growth and decay and weighting recency less strongly.

--- augur/src/process.py
+++ augur/src/process.py
@@ -33,10 +33,10 @@ virus_config = {
 							("AS", ["China", "SoutheastAsia", "JapanKorea"]), ("OC", ["Oceania"]) ],
 	'frequency_stiffness':5.0,
 	'verbose':2, 
-	'tol':1e-4, #tolerance for frequency optimization
+	'tol':2e-4, #tolerance for frequency optimization
 	'pc':1e-3, #pseudocount for frequencies 
 	'extra_pivots': 12,  # number of pivot point for or after the last observations of a mutations
-	'inertia':0.7,		# fraction of frequency change carry over in the stiffness term
+	'inertia':0.5,		# fraction of frequency change carry over in the stiffness term
 	'n_iqd':3,     # standard deviations from clock
 }
 
