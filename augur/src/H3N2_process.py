d3b1caaac1683c4a326dbeabcf5f81a662e0a286
Richard <richard.neher@tuebingen.mpg.de>
2015-11-27 09:55:03 +0100

    cherry picked changes

--- augur/src/H3N2_process.py
+++ augur/src/H3N2_process.py
@@ -285,7 +285,7 @@ class H3N2_process(process, H3N2_filter, H3N2_clean, H3N2_refine, HI_tree, fitne
 			self.filter()
 			if self.force_include is not None and os.path.isfile(self.force_include):
 				with open(self.force_include) as infile:
-					forced_strains = [fix_name(line.strip()).upper() for line in infile]
+					forced_strains = [fix_name(line.strip().split('\t')[0]).upper() for line in infile]
 			else:
 				forced_strains = []
 			self.subsample(viruses_per_month,
