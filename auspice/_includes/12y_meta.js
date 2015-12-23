8de6cb1c97d10a0cfa9e8ba12ce00063d3bd540d
Trevor Bedford <trvrb@trevorbedford.com>
2015-12-23 10:16:10 -0800

    Move resolution-specific JS params out of index.html to templates.

---
+++ auspice/_includes/12y_meta.js
@@ -0,0 +1,6 @@
+var time_window = 4.0;
+var full_data_time_window = 12.0;
+var time_ticks=[2004.0, 2006.0, 2008.0, 2010.0, 2012.0, 2014.0, 2016.0];
+var dfreq_dn = 2;
+var LBItime_window = 0.5;
+var LBItau = 0.0005;
---
+++ auspice/_includes/6y_meta.js
@@ -0,0 +1,6 @@
+var time_window = 2.0;
+var full_data_time_window = 6.0;
+var time_ticks=[2010.0, 2011.0, 2012.0, 2013.0, 2014.0, 2015.0, 2016.0];
+var dfreq_dn = 2;
+var LBItime_window = 0.5;
+var LBItau = 0.0005;
