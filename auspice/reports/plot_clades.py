5080cfbfc29e7bba195abb81b967b8c9c0a79b6c
Trevor Bedford <trvrb@trevorbedford.com>
2016-02-12 11:15:57 -0800

    Cleanup mutation plotting.

--- auspice/reports/plot_clades.py
+++ auspice/reports/plot_clades.py
@@ -19,25 +19,25 @@ if virus=='H3N2': ########## H3N2
     clades = ['3c2.a', '3c3.a', '3c3.b']
     mutations = ['HA1:114T','HA1:142K','HA1:171K', 'HA1:94H']
     clade_legend = {'panel':0, 'loc':3}
-    mut_legend = {'panel':1, 'loc':1}
+    mut_legend = {'panel':0, 'loc':3}
 elif virus=='H1N1pdm': ########## H1N1pdm
     freqs = json.load(open('../data/H1N1pdm_2y_frequencies.json'))
     clades = ['6b.1', '6b.2']
     mutations = ['HA1:84N','HA1:162N','HA1:152T', 'HA2:164G'] #these don't add up to one in Asia, probably due to sketchy sampling.
     clade_legend = {'panel':0, 'loc':3}
-    mut_legend = {'panel':1, 'loc':3}
+    mut_legend = {'panel':0, 'loc':3}
 elif virus=='Vic':
     freqs = json.load(open('../data/Vic_2y_frequencies.json'))
     clades = []
     mutations = ['HA1:129D', 'HA1:117V'] # HA1:56K would be good, but it currently isn't computed -> need to lower the threshold.
     clade_legend = {'panel':0, 'loc':3}
-    mut_legend = {'panel':1, 'loc':3}
+    mut_legend = {'panel':0, 'loc':3}
 elif virus=='Yam':
     freqs = json.load(open('../data/Yam_2y_frequencies.json'))
     clades = ['3', '2']
     mutations = ['HA1:251V', 'HA1:172Q']
     clade_legend = {'panel':0, 'loc':3}
-    mut_legend = {'panel':1, 'loc':4}
+    mut_legend = {'panel':0, 'loc':3}
 
 
 offset = datetime(2000,1,1).toordinal()
@@ -45,6 +45,8 @@ pivots = [offset+(x-2000)*365.25 for x in  freqs['clades']['global']['pivots']]
 regions = ['global', 'NA', 'AS', 'EU', 'OC']
 cols = sns.color_palette(n_colors=len(regions))
 fs=12
+months = MonthLocator(range(1, 13), bymonthday=1, interval=3)
+monthsFmt = DateFormatter("%b %y")
 
 if len(clades):
     fig, axs = plt.subplots(len(clades), 1, sharex=True, figsize=(8, len(clades)*2))
@@ -60,9 +62,10 @@ if len(clades):
         ax.set_ylim(0,1)
         ax.text(pivots[-1]-700, 0.9, clade)
         ax.tick_params(labelsize=fs)
-        ax.set_yticks([0,0.25, 0.5, 0.75, 1.0])
-        ax.fmt_xdata = DateFormatter('%Y-%m-%d')
-        fig.autofmt_xdate()
+        ax.set_yticks([0, 0.25, 0.5, 0.75, 1.0])
+        ax.xaxis.set_major_locator(months)
+        ax.xaxis.set_major_formatter(monthsFmt)
+    fig.autofmt_xdate()
     axs[clade_legend['panel']].legend(loc=clade_legend['loc'], ncol=1, bbox_to_anchor=(1.0, 0.2))
     #plt.tight_layout(h_pad=0.01)
     plt.subplots_adjust(left=0.08, right=0.86, top=0.96, bottom=0.11)
@@ -81,8 +84,12 @@ for mutation, ax in zip(mutations, axs):
     ax.set_xlim([pivots[-1]-700,pivots[-1]+30])
     ax.set_ylim(0,1)
     ax.text(pivots[-1]-700, 0.9, mutation)
-    ax.set_yticks([0,0.3, 0.6, 0.9])
+    ax.set_yticks([0, 0.25, 0.5, 0.75, 1.0])
     ax.tick_params(labelsize=fs)
-axs[mut_legend['panel']].legend(loc=mut_legend['loc'], ncol=2)
+    ax.xaxis.set_major_locator(months)
+    ax.xaxis.set_major_formatter(monthsFmt)
+fig.autofmt_xdate()    
+axs[mut_legend['panel']].legend(loc=mut_legend['loc'], ncol=1, bbox_to_anchor=(1.0, 0.2))
 #plt.tight_layout(h_pad=0.01)
+plt.subplots_adjust(left=0.08, right=0.86, top=0.96, bottom=0.11)
 plt.savefig('figures/feb-2016/'+virus+'_mutations.png')
