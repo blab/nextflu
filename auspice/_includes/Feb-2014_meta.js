8f1e45f23495241c30d06195cb2c98b958981fe2
Trevor Bedford <trevor@bedford.io>
2016-11-11 14:03:49 -0800

    Remove extraneous meta files

--- auspice/_includes/Feb-2014_meta.js
+++
@@ -1,6 +0,0 @@
-var time_window = 1.0;
-var full_data_time_window = 3.0;
-var time_ticks=[2011.5, 2012.0, 2012.5, 2013.0, 2013.5, 2014.0, 2014.5];
-var dfreq_dn = 2;
-var LBItime_window = 0.5;
-var LBItau = 0.0005;
