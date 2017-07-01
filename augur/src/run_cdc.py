b30a2f654722e9d3aa8ceedb0e7dc5545a1c2c3e
Trevor Bedford <trevor@bedford.io>
2017-07-01 15:52:45 -0700

    Include assay type and remove 12y

--- augur/src/run_cdc.py
+++ augur/src/run_cdc.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import os, re, time, datetime, csv, sys, argparse, subprocess
 
-def flu_build(lineage, resolution, passage):
+def flu_build(lineage, resolution, passage, assay):
 	'''
 	run build for single lineage / resolution / passage combination
 	'''
 	print '\n------------------------------\n'
-	print 'Processing lineage', lineage, 'with resolution', resolution, 'and passage', passage
+	print 'Processing lineage', lineage, 'with resolution', resolution, 'and passage', passage, 'and assay', assay
 	if resolution == '2y':
 		n_viruses = 100
 		n_years = 2
@@ -20,11 +20,11 @@ def flu_build(lineage, resolution, passage):
 		n_viruses = 12
 		n_years = 12
 	process = 'src/' + lineage + '_process.py'
-	HI_fname = 'data/' + lineage + '_cdc_hi_' + passage + '_titers.tsv'
-	force_include = 'data/' + lineage + '_cdc_hi_' + passage + '_strains.tsv'
+	HI_fname = 'data/' + lineage + '_cdc_' + assay + '_' + passage + '_titers.tsv'
+	force_include = 'data/' + lineage + '_cdc_' + assay + '_' + passage + '_strains.tsv'
 	call = map(str, [params.bin, process, '-v', n_viruses, '-y', n_years,
-		'--resolution', resolution + '_' + passage,
-		'--HI_fname', HI_fname, '--force_include', force_include,
+		'--resolution', resolution + '_' + passage + '_' + assay,
+		'--HI_fname', HI_fname.lower(), '--force_include', force_include.lower(),
 		'--skip', 'genotype_frequencies fitness HIvalidate'])
 	print call
 	subprocess.call(call)
@@ -37,18 +37,26 @@ if __name__=="__main__":
 	parser.add_argument('--flu_lineages', nargs='+', type = str,  help ="flu lineages to include")
 	parser.add_argument('--flu_resolutions', nargs='+', type = str,  help ="flu resolutions to include")
 	parser.add_argument('--flu_titers_passages', nargs='+', type = str,  help ="flu titers passages to include")
+	parser.add_argument('--flu_titers_assays', nargs='+', type = str,  help ="flu titers assays to include")
 	params = parser.parse_args()
 
 	if params.flu_lineages is None:
 		params.flu_lineages = ['H3N2', 'H1N1pdm', 'Vic', 'Yam']
 
 	if params.flu_resolutions is None:
-		params.flu_resolutions = ['2y', '3y', '6y', '12y']
+		params.flu_resolutions = ['2y', '3y', '6y']
 
 	if params.flu_titers_passages is None:
 		params.flu_titers_passages = ['cell', 'egg']
 
+	if params.flu_titers_assays is None:
+		params.flu_titers_assays = ['hi', 'fra']
+
 	for lineage in params.flu_lineages:
 		for resolution in params.flu_resolutions:
 			for passage in params.flu_titers_passages:
-				flu_build(lineage, resolution, passage)
+				for assay in params.flu_titers_assays:
+					if assay == "hi":
+						flu_build(lineage, resolution, passage, assay)
+					if assay == "fra" and lineage == "H3N2":
+						flu_build(lineage, resolution, passage, assay)
