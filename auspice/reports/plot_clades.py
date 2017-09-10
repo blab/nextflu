a45bdb347e20bec9b0eaa29b80e4f6e5e256dcaa
Richard Neher <richard.neher@unibas.ch>
2017-09-10 16:20:21 +0200

    tweaks to plot_clades to work with new augur format, fancy date labeling doesn't work right now, clades and regions labeling doesn't work either

--- auspice/reports/plot_clades.py
+++ auspice/reports/plot_clades.py
@@ -10,47 +10,47 @@ plt.ion()
 
 show_errorbars = True
 
-virus = 'H3N2'
-#virus = 'H1N1pdm'
-#virus = 'Vic'
-#virus = 'Yam'
+#virus = 'h3n2'
+virus = 'h1n1pdm'
+virus = 'vic'
+virus = 'yam'
 
 resolution = '2y'
-report = 'feb-2017'
+report = 'sep-2017'
 
 print "Plotting dataset " + virus + "/" + resolution + " for " + report + " report"
 
-freqs = json.load(open('../data/'+virus+'_'+resolution+'_frequencies.json'))
-counts = json.load(open('../data/'+virus+'_'+resolution+'_meta.json'))['virus_stats']
-region_names = json.load(open('../data/'+virus+'_'+resolution+'_meta.json'))['regions']
+freqs = json.load(open('../data/flu_'+virus+'_ha_'+resolution+'_cell_hi_frequencies.json'))
+counts = freqs['counts']
+pivots = freqs['pivots']
+meta = json.load(open('../data/flu_'+virus+'_ha_'+resolution+'_cell_hi_meta.json'))
+region_names = meta['geo']['region'].keys()
 region_codes = {'EU':['europe'], 'AS':['china', 'south_asia', 'japan_korea','southeast_asia'],
                 'NA':["north_america"], 'OC':["oceania"]}
 
-if virus=='H3N2': ########## H3N2
-    clades = ['3c2.a', '3c3.a', '3c3.b']
-    mutations = ['HA1:197K', 'HA1:171K', 'HA1:121K', 'HA1:142K', 'HA1:131K']
+if virus=='h3n2': ########## H3N2
+    clades = [] #'3c2.a', '3c3.a', '3c3.b']
+    mutations = ['HA1:135K', 'HA1:171K', 'HA1:121K', 'HA1:92R', 'HA1:131K', 'HA1:198P', 'HA1:91N']
     clade_legend = {'panel':0, 'loc':3}
     mut_legend = {'panel':0, 'loc':3}
-elif virus=='H1N1pdm': ########## H1N1pdm
-    clades = ['6b.1', '6b.2']
-    mutations = ['HA1:205K', 'HA1:183P', 'HA1:166V']
-    clade_legend = {'panel':0, 'loc':3}
-    mut_legend = {'panel':0, 'loc':3}
-elif virus=='Vic':
+elif virus=='h1n1pdm': ########## H1N1pdm
     clades = []
-    mutations = ['HA1:129D', 'HA1:117V'] # HA1:56K would be good, but it currently isn't computed -> need to lower the threshold.
+    mutations = ['HA1:164T', 'HA1:272I', 'HA1:295V']
     clade_legend = {'panel':0, 'loc':3}
     mut_legend = {'panel':0, 'loc':3}
-elif virus=='Yam':
-    clades = ['2', '3']
+elif virus=='vic':
+    clades = []
+    mutations = ['HA1:163-','HA1:129G', 'HA1:56K', 'HA1:209N', 'HA1:175V'] # HA1:56K would be good, but it currently isn't computed -> need to lower the threshold.
+    clade_legend = {'panel':0, 'loc':3}
+    mut_legend = {'panel':0, 'loc':3}
+elif virus=='yam':
+    clades = []
     mutations = ['HA1:172Q', 'HA1:251V', 'HA1:211R']
     clade_legend = {'panel':0, 'loc':3}
     mut_legend = {'panel':0, 'loc':3}
 
 
 offset = datetime(2000,1,1).toordinal()
-pivots = [offset+(x-2000)*365.25 for x in  freqs['clades']['global']['pivots']]
-pivots.pop()
 regions = ['global', 'NA', 'AS', 'EU', 'OC']
 region_label = {'global': 'Global', 'NA': 'N America', 'AS': 'Asia', 'EU': 'Europe', 'OC': 'Oceania'}
 cols = sns.color_palette(n_colors=len(regions))
@@ -63,36 +63,35 @@ monthsFmt = DateFormatter("%b")
 
 n=2
 n_std_dev=2
-l = len(freqs['clades']['global']['pivots'])
+l = len(pivots)
 bins = np.array([c[0] for c in counts])[-l:]
 date_bins = []
-for b in bins:
-	date_bins.append(datetime.strptime(b, "%Y-%m") - timedelta(days=8))
-count_array = np.array([c[1:] for c in counts])[-l:,:].T
-count_by_region = {region: np.sum([count_array[region_names.index(r)] for r in region_codes[region]], axis=0)
-                            for region in regions if region!='global'}
-count_by_region['global'] = count_array.sum(axis=0)
+# for b in bins:
+# 	date_bins.append(datetime.strptime(b, "%Y-%m") - timedelta(days=8))
+date_bins=pivots
+count_array = np.array([counts[c] for c in counts])[-l:,:].T
+count_by_region = {region: sum(x) for region,x in counts.iteritems() if region!='global'}
 smoothed_count_array = np.array([np.convolve(np.ones(n, dtype=float)/n, c, mode='same')
                         for c in count_array])
-smoothed_count_by_region = {region: np.sum([smoothed_count_array[region_names.index(r)] for r in region_codes[region]], axis=0)
-                            for region in regions if region!='global'}
-smoothed_count_by_region['global'] = smoothed_count_array.sum(axis=0)
+smoothed_count_by_region = {region: np.convolve(np.ones(n, dtype=float)/n, c, mode='same')
+                            for region, c in counts.iteritems()}
 
 print "Plotting sample counts"
 fig, ax = plt.subplots(figsize=(8, 3))
 drop = 3
 tmpcounts = np.zeros(len(date_bins[drop:]))
-plt.bar(date_bins[drop:], count_by_region['global'][drop:], width=18, linewidth=0, label="Other", color="#bbbbbb", clip_on=False)
+plt.bar(date_bins[drop:], counts['global'][drop:], width=0.07, linewidth=0, label="Other", color="#bbbbbb", clip_on=False)
 for c,region in zip(cols, regions):
     if region!='global':
-        plt.bar(date_bins[drop:], count_by_region[region][drop:], bottom=tmpcounts, width=18, linewidth=0, label=region_label[region], color=c, clip_on=False)
-        tmpcounts += count_by_region[region][drop:]
+        plt.bar(date_bins[drop:], counts[region][drop:], bottom=tmpcounts, width=.07, linewidth=0,
+                label=region_label[region], color=c, clip_on=False)
+        tmpcounts += np.array(counts[region][drop:])
 ax.tick_params(axis='x', which='major', labelsize=fs, pad=20)
 ax.tick_params(axis='x', which='minor', pad=7)
-ax.xaxis.set_major_locator(years)
-ax.xaxis.set_major_formatter(yearsFmt)
-ax.xaxis.set_minor_locator(months)
-ax.xaxis.set_minor_formatter(monthsFmt)
+# ax.xaxis.set_major_locator(years)
+# ax.xaxis.set_major_formatter(yearsFmt)
+# ax.xaxis.set_minor_locator(months)
+# ax.xaxis.set_minor_formatter(monthsFmt)
 ax.set_ylabel('Sample count')
 ax.legend(loc=3, ncol=1, bbox_to_anchor=(1.02, 0.53))
 plt.subplots_adjust(left=0.1, right=0.82, top=0.94, bottom=0.22)
@@ -103,17 +102,16 @@ if len(clades):
     print "Plotting clade frequencies"
     fig, axs = plt.subplots(len(clades), 1, sharex=True, figsize=(8, len(clades)*2))
     for clade, ax in zip(clades, axs):
-        for c,region in zip(cols, regions):
-            try:
-                tmp_freq = freqs['clades'][region][clade]
+        for c,region in zip(cols, ['global', 'north_america', 'asia', 'oceania']):
+            if True: #try:
+                tmp_freq = np.array(freqs['%s_%s'%(region, clade)])
                 if tmp_freq is not None:
-                    tmp_freq = np.array(tmp_freq[:-1])
-                    std_dev = np.sqrt(tmp_freq*(1-tmp_freq)/(smoothed_count_by_region[region][:-1]+1))
-                    ax.plot_date(pivots, tmp_freq,'-o', label = region_label[region], c=c, lw=3 if region=='global' else 1)
+                    std_dev = np.sqrt(tmp_freq*(1-tmp_freq)/(smoothed_count_by_region[region]+1))
+                    ax.plot(pivots, tmp_freq,'-o', label = region_label[region], c=c, lw=3 if region=='global' else 1)
                     if show_errorbars:
                         ax.fill_between(pivots, tmp_freq-n_std_dev*std_dev, tmp_freq+n_std_dev*std_dev, facecolor=c, linewidth=0, alpha=0.1)
-            except:
-                print "skipping", clade, region
+            # except:
+            #     print "skipping", clade, region
         ax.set_xlim([pivots[0], pivots[-1]])
         ax.set_ylim(0,1)
         ax.text(pivots[0]+5, 0.88, clade)
@@ -141,10 +139,9 @@ fig, axs = plt.subplots(len(mutations), 1, sharex=True, figsize=(8, len(mutation
 for mutation, ax in zip(mutations, axs):
     for c,region in zip(cols, regions):
         try:
-            tmp_freq = freqs['mutations'][region][mutation]
+            tmp_freq = np.array(freqs['%s_%s'%(region, mutation)])
             if tmp_freq is not None:
-                tmp_freq = np.array(tmp_freq[:-1])
-                std_dev = np.sqrt(tmp_freq*(1-tmp_freq)/(smoothed_count_by_region[region][:-1]+1))
+                std_dev = np.sqrt(tmp_freq*(1-tmp_freq)/(smoothed_count_by_region[region]+1))
                 ax.plot_date(pivots, tmp_freq, '-o', label = region_label[region], c=c, lw=3 if region=='global' else 1)
                 if show_errorbars:
                     ax.fill_between(pivots, tmp_freq-n_std_dev*std_dev, tmp_freq+n_std_dev*std_dev, facecolor=c, linewidth=0, alpha=0.1)
@@ -152,14 +149,14 @@ for mutation, ax in zip(mutations, axs):
             print "skipping", mutation, region
     ax.set_xlim([pivots[0], pivots[-1]])
     ax.set_ylim(0,1)
-    ax.text(pivots[0]+5, 0.88, mutation)
+    ax.text(pivots[0]+.1, 0.88, mutation)
     ax.set_yticklabels(['{:3.0f}%'.format(x*100) for x in [0, 0.2, 0.4, 0.6, 0.8, 1.0]])
     ax.tick_params(axis='x', which='major', labelsize=fs, pad=20)
     ax.tick_params(axis='x', which='minor', pad=7)
-    ax.xaxis.set_major_locator(years)
-    ax.xaxis.set_major_formatter(yearsFmt)
-    ax.xaxis.set_minor_locator(months)
-    ax.xaxis.set_minor_formatter(monthsFmt)
+    # ax.xaxis.set_major_locator(years)
+    # ax.xaxis.set_major_formatter(yearsFmt)
+    # ax.xaxis.set_minor_locator(months)
+    # ax.xaxis.set_minor_formatter(monthsFmt)
 fig.autofmt_xdate(bottom=0.25, rotation=0, ha='center')
 fax = fig.add_axes( [0., 0., 1, 1] )
 fax.set_axis_off()
