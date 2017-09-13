91cda05fe045dc70b269149566b04168ae7671d1
Richard Neher <richard.neher@unibas.ch>
2017-09-13 11:21:43 +0200

    spec LBI params in meta

--- auspice/_includes/2y_meta.js
+++ auspice/_includes/2y_meta.js
@@ -1,5 +1,6 @@
+var time_window = 2.0;
 var full_data_time_window = 2.0;
-var time_ticks=[2016.0, 2016.5, 2017.0, 2017.5];
-var dfreq_dn = 2;
-var LBItime_window = 0.5;
-var LBItau = 0.0005;
+var time_ticks=[2014.0, 2014.5, 2015.0, 2015.5, 2016.0, 2016.5, 2017.0, 2017.5];
+var dfreq_dn = 4;
+var LBItime_window = 0.75;
+var LBItau = 0.25;
