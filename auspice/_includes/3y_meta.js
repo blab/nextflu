b1198d16d0fbda199c599b2503af4b25fa48d00f
Trevor Bedford <trevor@bedford.io>
2017-09-13 09:31:59 -0400

    Clean up virus index files

--- auspice/_includes/3y_meta.js
+++ auspice/_includes/3y_meta.js
@@ -4,4 +4,3 @@ var time_ticks=[2014.0, 2014.5, 2015.0, 2015.5, 2016.0, 2016.5, 2017.0, 2017.5];
 var dfreq_dn = 4;
 var LBItime_window = 0.75;
 var LBItau = 0.25;
-
--- auspice/_includes/6y_meta.js
+++ auspice/_includes/6y_meta.js
@@ -4,4 +4,3 @@ var time_ticks=[2010.0, 2011.0, 2012.0, 2013.0, 2014.0, 2015.0, 2016.0, 2017.0];
 var dfreq_dn = 6;
 var LBItime_window = 0.75;
 var LBItau = 0.25;
-
