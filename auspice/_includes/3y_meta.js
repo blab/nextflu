141296270a392c176d09f3cb0b2adca8cf586ba3
Richard Neher <richard.neher@unibas.ch>
2017-09-19 10:41:34 +0200

    tweaked LBI params

--- auspice/_includes/3y_meta.js
+++ auspice/_includes/3y_meta.js
@@ -2,5 +2,5 @@ var time_window = 3.0;
 var full_data_time_window = 3.0;
 var time_ticks=[2014.0, 2014.5, 2015.0, 2015.5, 2016.0, 2016.5, 2017.0, 2017.5];
 var dfreq_dn = 6;
-var LBItime_window = 0.75;
-var LBItau = 0.25;
+var LBItime_window = 0.6;
+var LBItau = 0.4;
--- auspice/js/auspice/colors.js
+++ auspice/js/auspice/colors.js
@@ -30,7 +30,8 @@ var receptorBindingColorScale = d3.scale.linear().clamp([true])
 	.range(colors[4]);
 
 var lbiColorScale = d3.scale.linear()
-	.domain([0.0, 0.02, 0.04, 0.07, 0.1, 0.2, 0.4, 0.7, 0.9, 1.0])
+//	.domain([0.0, 0.02, 0.04, 0.07, 0.1, 0.2, 0.4, 0.7, 0.9, 1.0])
+	.domain([0.0, 0.03, 0.06, 0.12, 0.20, 0.3, 0.4, 0.5, 0.6, 0.7])
 	.range(colors[10]);
 
 var dfreqColorScale = d3.scale.linear()
