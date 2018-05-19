b6414437ee4cf4a332368d23d3e6e1748c06ddee
Richard Neher <richard.neher@unibas.ch>
2018-05-19 21:47:02 +0200

    update tick locations on frequency plot

--- auspice/_includes/3y_meta.js
+++ auspice/_includes/3y_meta.js
@@ -1,6 +1,6 @@
 var time_window = 3.0;
 var full_data_time_window = 3.0;
-var time_ticks=[2014.0, 2014.5, 2015.0, 2015.5, 2016.0, 2016.5, 2017.0, 2017.5];
+var time_ticks=[2014.0, 2014.5, 2015.0, 2015.5, 2016.0, 2016.5, 2017.0, 2017.5, 2018.0, 2018.5];
 var dfreq_dn = 6;
 var LBItime_window = 0.6;
 var LBItau = 0.4;
